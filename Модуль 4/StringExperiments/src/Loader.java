eace0837bbdf4e8cc7b2594227cb7a73bb21903a
CuttySark-prog <AppMarinaDev@gmail.com>
2020-03-10 13:25:17 +0300

    не получилось

--- Модуль 4/StringExperiments/src/Loader.java
+++ Модуль 4/StringExperiments/src/Loader.java
@@ -7,23 +7,27 @@ public class Loader
     public static void main(String[] args)
     {
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-        String salary = text.replaceAll("[^0-9]", " ");
-        System.out.println(salary);
-        String[] salarys = salary.split("\\s+");
-        int sum = 0;
-        for (int i = 0; i < salarys.length; i++)
-        {
-          System.out.println(salarys[i]);
-          sum = sum + Integer.parseInt(salarys[i]);
+//        String salary = text.replaceAll("[^0-9]", " ");
+//        String sal = salary.trim();
+//        String[] salarys = sal.split("\\s+");
+        //Вариант 2
+        var m = Pattern.compile("(\\d+)(\\d+)(\\d+)").matcher(text);
+        if (m.find()) {
+            System.out.println("Зарплата Васи: " + m.group(1));
+            System.out.println("Зарплата Пети: " + m.group(2));
+            System.out.println("Зарплата Маши: " + m.group(3));
+            System.out.println("Все вместе заработали: " + (Integer.parseInt(m.group(1))+Integer.parseInt(m.group(2)) + Integer.parseInt(m.group(3))));
         }
 
-//   //     int numArr[] = new int[salarys.length];
-//   //     for (int i = 0; i < salarys.length; i++) {
-//   //         numArr[i] = Integer.parseInt(salarys[i]);
-//   //         System.out.println(numArr[i]);
-//   //     }
-
-        System.out.println(text);
-        System.out.println("Вася и Маша в сумме заработали: " + sum);
+            //Вариант 1
+//        int sum = 0;
+//        for (int i = 0; i < salarys.length; i++)
+//        {
+//          System.out.println(salarys[i]);
+//          sum = sum + Integer.parseInt(salarys[i]);
+//        }
+//
+//        System.out.println(text);
+//        System.out.println("Вася и Маша в сумме заработали: " + sum);
     }
 }
--- Модуль 4/Telephone number/src/Loader.java
+++ Модуль 4/Telephone number/src/Loader.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Loader
 {
@@ -7,36 +9,47 @@ public class Loader
         System.out.println("Введите номер телефона: ");
         Scanner enterNumber = new Scanner(System.in);
         String number = enterNumber.nextLine();
-        String cod, cod1, cod2, cod3;
+        //Версия 2
 
-        String sortNumber = number.replaceAll("[^0-9]", "");
-
-        System.out.println(sortNumber);
-
-        //char first = sortNumber.charAt(0);
-
-        if (sortNumber.length() == 11)
+        Pattern pattern = Pattern.compile("(//+7,8)?([0-9]{3})([0-9]{3})([0-9]{2}])([0-9]{2})");
+        Matcher matcher = pattern.matcher(number);
+      //  String num  =  matcher.replaceAll( "s");
+        var m = pattern.compile("(/+7,/8)?([0-9]{3})([0-9]{3})([0-9]{2}])([0-9]{2})").matcher(number);
+        if (m.find())
         {
-            cod = sortNumber.substring(1,4);
-            cod1 = sortNumber.substring(4,7);
-            cod2 = sortNumber.substring(7,9);
-            cod3 = sortNumber.substring(9);
-            System.out.printf("+7 %s %s-%s-%s", cod, cod1, cod2, cod3);
+            System.out.println("Вы ввели");
+            System.out.printf("+7 %s %s-%s-%s", m.group(2), m.group(3), m.group(4), m.group(5));
         }
-
-        else if (sortNumber.length() == 10)
-        {
-            cod = sortNumber.substring(0,2);
-            cod1 = sortNumber.substring(3,5);
-            cod2 = sortNumber.substring(6,7);
-            cod3 = sortNumber.substring(8,9);
-            System.out.printf("+7 %s %s-%s-%s", cod, cod1, cod2, cod3);
-        }
-        else
-        {
-            System.out.println("Номер введен неполностью");
-        }
-
-        //  System.out.printf("+7 %s %s-%s-%s", cod, cod1, cod2, cod3);
+        else System.out.println("Что-то пошло не так");
+        //Версия 1
+//        String cod, cod1, cod2, cod3;
+//
+//        String sortNumber = number.replaceAll("[^0-9]", "");
+//
+//        System.out.println(sortNumber);
+//
+//        if (sortNumber.length() == 11)
+//        {
+//            cod = sortNumber.substring(1,4);
+//            cod1 = sortNumber.substring(4,7);
+//            cod2 = sortNumber.substring(7,9);
+//            cod3 = sortNumber.substring(9);
+//            System.out.printf("+7 %s %s-%s-%s", cod, cod1, cod2, cod3);
+//        }
+//
+//        else if (sortNumber.length() == 10)
+//        {
+//            cod = sortNumber.substring(0,2);
+//            cod1 = sortNumber.substring(3,5);
+//            cod2 = sortNumber.substring(6,7);
+//            cod3 = sortNumber.substring(8,9);
+//            System.out.printf("+7 %s %s-%s-%s", cod, cod1, cod2, cod3);
+//        }
+//        else
+//        {
+//            System.out.println("Номер введен неполностью");
+//        }
+//
+//        //  System.out.printf("+7 %s %s-%s-%s", cod, cod1, cod2, cod3);
     }
 }
