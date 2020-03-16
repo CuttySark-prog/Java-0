7f698a9ff33bf9a23622df0234ab6fd08fd34bbc
CuttySark-prog <AppMarinaDev@gmail.com>
2020-03-16 09:13:02 +0300

    д.з.4.5

--- Модуль 4/Birthday/src/Loader.java
+++ Модуль 4/Birthday/src/Loader.java
@@ -1,9 +1,24 @@
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 
 public class Loader
 {
-    public static void main (String args)
+    public static void main(String[] args)
+    {
+        LocalDate birthdayDate = LocalDate.of(1986, 8, 25);
+        LocalDate date = LocalDate.now();
+        Period age = Period.between(birthdayDate,date);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        System.out.println(age.getYears());
+
+        for (int i = 0; i <= age.getYears(); i++)
         {
             
+            System.out.printf("%d - %s - %s%n", i, birthdayDate.format(formatter), birthdayDate.getDayOfWeek());
+            birthdayDate = birthdayDate.plusYears(1);
         }
+    }
 }
