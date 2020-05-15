0ea9ad4cd3b3101cc24c42170724867b3a42bf97
CuttySark-prog <AppMarinaDev@gmail.com>
2020-05-15 12:17:54 +0300

    обработка замечаний

--- 05_ArraysAndCollections/E-mail_book/src/Loader.java
+++ 05_ArraysAndCollections/E-mail_book/src/Loader.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.regex.Matcher;
@@ -7,42 +6,40 @@ import java.util.regex.Pattern;
 public class Loader
 {
     private static HashSet<String> book = new HashSet<>();
+    private static void printList()
+    {
+        for (String item : book) {
+            System.out.println(item);
+        }
+    }
     public static void main(String args[])
     {
         System.out.printf("ВВедите электронный адрес%n");
-        Scanner enterAddress;
         while (true)
         {
-            enterAddress = new Scanner(System.in);
+            Scanner enterAddress = new Scanner(System.in);
             String address = enterAddress.nextLine();
             Matcher matchAddress = Pattern.compile("^[a-zA-Z0-9-]+@[a-z]+\\.[a-z]+").matcher(address);
             Matcher matchExit = Pattern.compile("EXIT").matcher(address);
-            Matcher matchList = Pattern.compile("^LIST").matcher(address);
-            if (matchList.matches())
-            {
-                for (String item : book) {
-                    System.out.println(item);
-                }
-            }
-             else if (matchAddress.matches())
+            Matcher matchList = Pattern.compile("LIST").matcher(address);
+
+            if (matchAddress.matches())
             {
                 book.add(matchAddress.group());
-               for (String book: book)
-               {
-                   System.out.println(book);
-               }
+                printList();
             }
-             else if (matchExit.matches())
+            else if (matchList.matches())
+            {
+                printList();
+            }
+            else if (matchExit.matches())
             {
                 break;
             }
             else
             {
                 System.out.println("Неправильный адрес");
-                continue;
             }
-
-
         }
     }
 }
