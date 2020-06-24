2a83139456c7c3c7252effcd880ae65f332f7c12
CuttySark-prog <AppMarinaDev@gmail.com>
2020-06-24 09:44:05 +0300

    работает, но как-то не так

--- 05_ArraysAndCollections/Search Speed ​​Comparison/src/Loader.java
+++ 05_ArraysAndCollections/Search Speed ​​Comparison/src/Loader.java
@@ -1,11 +1,11 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Loader
 {
-    public static final ArrayList<String> carNumberList = new ArrayList<String>();
-    public static final String[] letters = {"C", "M", "T", "B", "A", "P", "O", "H", "E", "Y"};
+    public static ArrayList<String> carNumberArrayList = new ArrayList<>();
+    public static TreeSet<String> carNumberTreeSet = new TreeSet<>();
+    public static HashSet<String> carNumberHashSet = new HashSet<>();
+    public static final String[] letters = { "A", "B", "C", "E", "H", "M", "O", "P", "T", "Y"};
     public static List<String> generateList()
     {
         System.out.println("генерация");
@@ -14,40 +14,111 @@ public class Loader
 //        XNNNYZR - пример, A111BC197, Y777HC66
 
 
-        for (int i = 1; i <= 199;i++)
+        for (int x = 0; x< letters.length; x++)
         {
-            if (i <= 9)
+            for (int y =0; y < letters.length; y++)
             {
-                for (int y = 1; y <letters.length; y++)
+                for (int z = 0; z < letters.length; z++)
                 {
-                    String number = letters[y-1]+y+y+y+letters[y-1]+letters[y-1]+"0"+i;
-                    carNumberList.add(number);
+                    for (int i = 1; i <= 199; i++)
+                    {
+                        if (i <= 9) {
+                            for (int j = 1; j <= 9; j++) {
+                                String number = letters[x] + j + j + j + letters[y] + letters[z] + "0" + i;
+                                carNumberArrayList.add(number);
+                            }
+                        }
+                        if (i > 9) {
+                            for (int j = 1; j <= 9; j++) {
+                                String number = letters[x] + j + j + j + letters[y] + letters[z] + i;
+                                carNumberArrayList.add(number);
+                            }
+                        }
+                    }
                 }
-            }
-            else if (i >9)
-            {
-                for (int y = 1; y <letters.length; y++)
-                {
-                    String number = letters[y-1]+y+y+y+letters[y-1]+letters[y-1]+i;
-                    carNumberList.add(number);
-                }
-            }
-            else
-            {
-                System.out.println("В разработке");
             }
         }
-        return carNumberList;
+        return carNumberArrayList;
     }
+
     public static void main (String[] args)
     {
         generateList();
-        int g = 0;
-        for (String number: carNumberList)
+        carNumberHashSet.addAll(carNumberArrayList);
+        carNumberTreeSet.addAll(carNumberArrayList);
+//        int g = 0;
+//        for (String number: carNumberArrayList)
+//        {
+//            System.out.println(number);
+//            g+=1;
+//        }
+//        System.out.println(g);
+        Scanner scanner = new Scanner(System.in);
+        for(;;)
         {
-            System.out.println(number);
-            g+=1;
+            String findNumber = scanner.next();
+            directSearchByArrayList(findNumber);
+            binarySearchOnSortedArrayList(findNumber);
+            searchInHashSet(findNumber);
+            searchInTreeSet(findNumber);
         }
-        System.out.println(g);
     }
+    public static void directSearchByArrayList(String s)
+    {
+        long start = System.currentTimeMillis();
+        if (carNumberArrayList.contains(s))
+        {
+            long duration = System.currentTimeMillis() - start;
+            System.out.println("Поиск перебором: номер найден, поиск занял" + " " + duration + "нс");
+        }
+        else
+        {
+            long duration = System.currentTimeMillis() - start;
+            System.out.println("Поиск перебором: номер не найден, поиск занял" + " " + duration + "нс");
+        }
+    }
+    public static void binarySearchOnSortedArrayList(String s)
+    {
+        long start = System.currentTimeMillis();
+        if (Collections.binarySearch(carNumberArrayList,s)>0)
+        {
+            long duration = System.currentTimeMillis() - start;
+            System.out.println("Поиск перебором: номер найден, поиск занял" + " " + duration + "нс");
+        }
+        else
+        {
+            long duration = System.currentTimeMillis() - start;
+            System.out.println("Бинарный поиск: номер не найден, поиск занял" + " " + duration + "нс");
+        }
+    }
+    public static void searchInHashSet(String s)
+    {
+        long start = System.currentTimeMillis();
+        if (carNumberHashSet.contains(s))
+        {
+            long duration = System.currentTimeMillis() - start;
+            System.out.println("Поиск в HashSet: номер найден, поиск занял" + " " + duration + "нс");
+        }
+        else
+        {
+            long duration = System.currentTimeMillis() - start;
+            System.out.println("Поиск в HashSet: номер не найден, поиск занял" + " " + duration + "нс");
+        }
+
+    }
+    public static void searchInTreeSet(String s)
+    {
+        long start = System.currentTimeMillis();
+        if (carNumberHashSet.contains(s))
+        {
+            long duration = System.currentTimeMillis() - start;
+            System.out.println("Поиск в TreeSet: номер найден, поиск занял" + " " + duration + "нс");
+        }
+        else
+        {
+            long duration = System.currentTimeMillis() - start;
+            System.out.println("Поиск в TreeSet: номер не найден, поиск занял" + " " + duration + "нс");
+        }
+    }
+
 }
