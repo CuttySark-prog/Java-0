fef9c1ca6f60f12c53d53857e81f5590a5cf5445
CuttySark-prog <appmarinadev@gmail.com>
2020-09-29 17:00:23 +0500

    Test

--- 06_InheritanceAndPolymorphism/Company management/src/Loader.java
+++ 06_InheritanceAndPolymorphism/Company management/src/Loader.java
@@ -5,6 +5,8 @@ public class Loader
     public static void main (String[] args)
     {
         Manager tab1 = new Manager("Peter", 45000);
+        Manager tab2 = new Manager("Sofi", 33000);
         tab1.getMonthSalary();
     }
+
 }
